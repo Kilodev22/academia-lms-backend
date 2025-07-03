78e71635dae9a92e75c446bfa6a7c342d8f2d185
kilodev22 <superricas2296@gmail.com>
2025-07-03 16:27:38 -0500

    Añadir print de depuración para DATABASE_URL

--- config.py
+++ config.py
@@ -7,12 +7,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-    # Clave para firmar los tokens de la API
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
-    
-    # NUEVO: Clave para firmar la sesión de Flask (usada por Flask-Admin)
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    
+    # --- AÑADE ESTAS LÍNEAS PARA DEPURAR ---
+    db_url = os.environ.get('DATABASE_URL')
+    print(f"DEBUG: La DATABASE_URL leída es: {db_url}") 
     # Configuración de la base de datos
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
