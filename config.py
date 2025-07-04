096f7e681e8947bfa5c3381f2c0b0c6e5279ba40
kilodev22 <superricas2296@gmail.com>
2025-07-04 14:08:59 -0500

    Revert "Añadir print de depuración para DATABASE_URL"
    This reverts commit 78e71635dae9a92e75c446bfa6a7c342d8f2d185.

--- config.py
+++ config.py
@@ -7,12 +7,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # Clave para firmar los tokens de la API
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     
+<<<<<<< HEAD
     # --- AÑADE ESTAS LÍNEAS PARA DEPURAR ---
     db_url = os.environ.get('DATABASE_URL')
     if db_url and db_url.startswith("mysql://"):
      db_url = db_url.replace("mysql://", "mysql+mysqlconnector://", 1)
+=======
+    # NUEVO: Clave para firmar la sesión de Flask (usada por Flask-Admin)
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+>>>>>>> parent of 78e7163 (Añadir print de depuración para DATABASE_URL)
 
     # Configuración de la base de datos
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
